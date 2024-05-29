53fa8fa87a1ee7bcddf2870f99f26eebdf3cd568
Ashithshiman <ashithshiman@hotmail.com>
2024-05-29 23:54:38 +0530

    navbar-globe icone has been removed

--- resources/src/containers/layouts/largeSidebar/TopNav.vue
+++ resources/src/containers/layouts/largeSidebar/TopNav.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="main-header">
-    <div class="logo">
-       <router-link to="/app/dashboard">
-        <!-- <img :src="'/images/'+currentUser.logo" alt width="60" height="60"> -->
-       </router-link>
-    </div>
+    <div class="main-header">
+        <div class="logo">
+            <router-link to="/app/dashboard">
+                <!-- <img :src="'/images/'+currentUser.logo" alt width="60" height="60"> -->
+            </router-link>
+        </div>
 
-    <div @click="sideBarToggle" class="menu-toggle">
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+        <div @click="sideBarToggle" class="menu-toggle">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
 
-    <div style="margin: auto"></div>
+        <div style="margin: auto"></div>
 
-    <div class="header-part-right">
-      <router-link 
-        v-if="currentUserPermissions && currentUserPermissions.includes('Pos_view')"
-        class="btn btn-outline-primary tn-sm btn-rounded"
-        to="/app/pos"
-      >
-      <span class="ul-btn__text ml-1">POS</span>
-      </router-link>
-      <!-- Full screen toggle -->
-      <i class="i-Full-Screen header-icon d-none d-sm-inline-block" @click="handleFullScreen"></i>
-      <!-- Grid menu Dropdown -->
+        <div class="header-part-right">
+            <router-link
+                v-if="
+                    currentUserPermissions &&
+                    currentUserPermissions.includes('Pos_view')
+                "
+                class="btn btn-outline-primary tn-sm btn-rounded"
+                to="/app/pos"
+            >
+                <span class="ul-btn__text ml-1">POS</span>
+            </router-link>
+            <!-- Full screen toggle -->
+            <i
+                class="i-Full-Screen header-icon d-none d-sm-inline-block"
+                @click="handleFullScreen"
+            ></i>
+            <!-- Grid menu Dropdown -->
 
-      <div class="dropdown">
+            <!-- <div class="dropdown">
         <b-dropdown
           id="dropdown"
           text="Dropdown Button"
@@ -137,86 +143,125 @@
             </div>
           </vue-perfect-scrollbar>
         </b-dropdown>
-      </div>
-      <!-- Notificaiton -->
-      <div class="dropdown">
-        <b-dropdown
-          id="dropdown-1" 
-          text="Dropdown Button"
-          class="m-md-2 badge-top-container d-none  d-sm-inline-block"
-          toggle-class="text-decoration-none"
-          no-caret
-          variant="link"
-        >
-          <template slot="button-content" >
-            <span class="badge badge-primary" v-if="notifs_alert > 0">1</span>
-            <i class="i-Bell text-muted header-icon"></i>
-          </template>
-          <!-- Notification dropdown -->
-          <vue-perfect-scrollbar
-            :settings="{ suppressScrollX: true, wheelPropagation: false }"
-            :class="{ open: getSideBarToggleProperties.isSideNavOpen }"
-            ref="myData"
-            class="dropdown-menu-right rtl-ps-none notification-dropdown ps scroll"
-          >
-            <div class="dropdown-item d-flex" v-if="notifs_alert > 0">
-              <div class="notification-icon">
-                <i class="i-Bell text-primary mr-1"></i>
-              </div>
-              <div class="notification-details flex-grow-1"
-              v-if="currentUserPermissions && currentUserPermissions.includes('Reports_quantity_alerts')">
-               <router-link  tag="a" to="/app/reports/quantity_alerts" >
-                <p class="text-small text-muted m-0">
-                  {{notifs_alert}} {{$t('ProductQuantityAlerts')}}
-                  </p>
-               </router-link>
-              </div>
+      </div> -->
+            <!-- Notificaiton -->
+            <div class="dropdown">
+                <b-dropdown
+                    id="dropdown-1"
+                    text="Dropdown Button"
+                    class="m-md-2 badge-top-container d-none d-sm-inline-block"
+                    toggle-class="text-decoration-none"
+                    no-caret
+                    variant="link"
+                >
+                    <template slot="button-content">
+                        <span
+                            class="badge badge-primary"
+                            v-if="notifs_alert > 0"
+                            >1</span
+                        >
+                        <i class="i-Bell text-muted header-icon"></i>
+                    </template>
+                    <!-- Notification dropdown -->
+                    <vue-perfect-scrollbar
+                        :settings="{
+                            suppressScrollX: true,
+                            wheelPropagation: false,
+                        }"
+                        :class="{
+                            open: getSideBarToggleProperties.isSideNavOpen,
+                        }"
+                        ref="myData"
+                        class="dropdown-menu-right rtl-ps-none notification-dropdown ps scroll"
+                    >
+                        <div
+                            class="dropdown-item d-flex"
+                            v-if="notifs_alert > 0"
+                        >
+                            <div class="notification-icon">
+                                <i class="i-Bell text-primary mr-1"></i>
+                            </div>
+                            <div
+                                class="notification-details flex-grow-1"
+                                v-if="
+                                    currentUserPermissions &&
+                                    currentUserPermissions.includes(
+                                        'Reports_quantity_alerts'
+                                    )
+                                "
+                            >
+                                <router-link
+                                    tag="a"
+                                    to="/app/reports/quantity_alerts"
+                                >
+                                    <p class="text-small text-muted m-0">
+                                        {{ notifs_alert }}
+                                        {{ $t("ProductQuantityAlerts") }}
+                                    </p>
+                                </router-link>
+                            </div>
+                        </div>
+                    </vue-perfect-scrollbar>
+                </b-dropdown>
             </div>
-           
-          </vue-perfect-scrollbar>
-        </b-dropdown>
-      </div>
-      <!-- Notificaiton End -->
+            <!-- Notificaiton End -->
 
-      <!-- User avatar dropdown -->
-      <div class="dropdown">
-        <b-dropdown
-          id="dropdown-1"
-          text="Dropdown Button"
-          class="m-md-2 user col align-self-end d-md-block"
-          toggle-class="text-decoration-none"
-          no-caret
-          variant="link"
-        >
-          <template slot="button-content" >
-            <img
-              :src="'/images/avatar/'+currentUser.avatar"
-              id="userDropdown"
-              alt
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-          </template>
+            <!-- User avatar dropdown -->
+            <div class="dropdown">
+                <b-dropdown
+                    id="dropdown-1"
+                    text="Dropdown Button"
+                    class="m-md-2 user col align-self-end d-md-block"
+                    toggle-class="text-decoration-none"
+                    no-caret
+                    variant="link"
+                >
+                    <template slot="button-content">
+                        <img
+                            :src="'/images/avatar/' + currentUser.avatar"
+                            id="userDropdown"
+                            alt
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        />
+                    </template>
 
-          <div class="dropdown-menu-right" aria-labelledby="userDropdown">
-            <div class="dropdown-header">
-              <i class="i-Lock-User mr-1"></i> <span >{{currentUser.username}}</span>
+                    <div
+                        class="dropdown-menu-right"
+                        aria-labelledby="userDropdown"
+                    >
+                        <div class="dropdown-header">
+                            <i class="i-Lock-User mr-1"></i>
+                            <span>{{ currentUser.username }}</span>
+                        </div>
+                        <router-link to="/app/profile" class="dropdown-item">{{
+                            $t("profil")
+                        }}</router-link>
+                        <router-link
+                            v-if="
+                                currentUserPermissions &&
+                                currentUserPermissions.includes(
+                                    'setting_system'
+                                )
+                            "
+                            to="/app/settings/System_settings"
+                            class="dropdown-item"
+                            >{{ $t("Settings") }}</router-link
+                        >
+                        <a
+                            class="dropdown-item"
+                            href="#"
+                            @click.prevent="logoutUser"
+                            >{{ $t("logout") }}</a
+                        >
+                    </div>
+                </b-dropdown>
             </div>
-            <router-link to="/app/profile" class="dropdown-item">{{$t('profil')}}</router-link>
-            <router-link
-              v-if="currentUserPermissions && currentUserPermissions.includes('setting_system')"
-              to="/app/settings/System_settings"
-              class="dropdown-item"
-            >{{$t('Settings')}}</router-link>
-            <a class="dropdown-item" href="#" @click.prevent="logoutUser">{{$t('logout')}}</a>
-          </div>
-        </b-dropdown>
-      </div>
+        </div>
     </div>
-  </div>
 
-  <!-- header top menu end -->
+    <!-- header top menu end -->
 </template>
 <script>
 import Util from "./../../../utils";
@@ -228,126 +273,116 @@ import { mixin as clickaway } from "vue-clickaway";
 import FlagIcon from "vue-flag-icon";
 
 export default {
-  mixins: [clickaway],
-  components: {
-    FlagIcon
-  },
-
-  data() {
-  
-    return {
-      langs: [
-        "en",
-        "fr",
-        "ar",
-        "de",
-        "es",
-        "it",
-        "Ind",
-        "thai",
-        "tr_ch",
-        "sm_ch",
-        "tur",
-        "ru",
-        "hn",
-        "vn",
-        "kr",
-        "ba",
-        "br",
-        "da",
-      ],
-      
-      isDisplay: true,
-      isStyle: true,
-      isSearchOpen: false,
-      isMouseOnMegaMenu: true,
-      isMegaMenuOpen: false,
-      is_Load:false,
-     
-    };
-  },
- 
-   computed: {
-     
-     ...mapGetters([
-       "currentUser",
-      "getSideBarToggleProperties",
-      "currentUserPermissions",
-      "notifs_alert",
-    ]),
-
-
-  },
-
-  methods: {
-    
-    ...mapActions([
-      "changeSecondarySidebarProperties",
-      "changeSidebarProperties",
-      "changeThemeMode",
-      "logout",
-    ]),
-
-    logoutUser() {
-      this.$store.dispatch("logout");
+    mixins: [clickaway],
+    components: {
+        FlagIcon,
     },
 
-    SetLocal(locale) {
-      this.$i18n.locale = locale;
-      this.$store.dispatch("language/setLanguage", locale);
-      Fire.$emit("ChangeLanguage");
+    data() {
+        return {
+            langs: [
+                "en",
+                "fr",
+                "ar",
+                "de",
+                "es",
+                "it",
+                "Ind",
+                "thai",
+                "tr_ch",
+                "sm_ch",
+                "tur",
+                "ru",
+                "hn",
+                "vn",
+                "kr",
+                "ba",
+                "br",
+                "da",
+            ],
+
+            isDisplay: true,
+            isStyle: true,
+            isSearchOpen: false,
+            isMouseOnMegaMenu: true,
+            isMegaMenuOpen: false,
+            is_Load: false,
+        };
     },
 
-    handleFullScreen() {
-      Util.toggleFullScreen();
-    },
-    logoutUser() {
-      this.logout();
-    },
-
-    closeMegaMenu() {
-      this.isMegaMenuOpen = false;
-    },
-    toggleMegaMenu() {
-      this.isMegaMenuOpen = !this.isMegaMenuOpen;
-    },
-    toggleSearch() {
-      this.isSearchOpen = !this.isSearchOpen;
+    computed: {
+        ...mapGetters([
+            "currentUser",
+            "getSideBarToggleProperties",
+            "currentUserPermissions",
+            "notifs_alert",
+        ]),
     },
 
-    sideBarToggle(el) {
-      if (
-        this.getSideBarToggleProperties.isSideNavOpen &&
-        this.getSideBarToggleProperties.isSecondarySideNavOpen &&
-        isMobile
-      ) {
-        this.changeSidebarProperties();
-        this.changeSecondarySidebarProperties();
-      } else if (
-        this.getSideBarToggleProperties.isSideNavOpen &&
-        this.getSideBarToggleProperties.isSecondarySideNavOpen
-      ) {
-        this.changeSecondarySidebarProperties();
-      } else if (this.getSideBarToggleProperties.isSideNavOpen) {
-        this.changeSidebarProperties();
-      } else if (
-        !this.getSideBarToggleProperties.isSideNavOpen &&
-        !this.getSideBarToggleProperties.isSecondarySideNavOpen &&
-        !this.getSideBarToggleProperties.isActiveSecondarySideNav
-      ) {
-        this.changeSidebarProperties();
-      } else if (
-        !this.getSideBarToggleProperties.isSideNavOpen &&
-        !this.getSideBarToggleProperties.isSecondarySideNavOpen
-      ) {
+    methods: {
+        ...mapActions([
+            "changeSecondarySidebarProperties",
+            "changeSidebarProperties",
+            "changeThemeMode",
+            "logout",
+        ]),
 
-        this.changeSidebarProperties();
-        this.changeSecondarySidebarProperties();
-      }
-    }
-  }
+        logoutUser() {
+            this.$store.dispatch("logout");
+        },
+
+        SetLocal(locale) {
+            this.$i18n.locale = locale;
+            this.$store.dispatch("language/setLanguage", locale);
+            Fire.$emit("ChangeLanguage");
+        },
+
+        handleFullScreen() {
+            Util.toggleFullScreen();
+        },
+        logoutUser() {
+            this.logout();
+        },
+
+        closeMegaMenu() {
+            this.isMegaMenuOpen = false;
+        },
+        toggleMegaMenu() {
+            this.isMegaMenuOpen = !this.isMegaMenuOpen;
+        },
+        toggleSearch() {
+            this.isSearchOpen = !this.isSearchOpen;
+        },
+
+        sideBarToggle(el) {
+            if (
+                this.getSideBarToggleProperties.isSideNavOpen &&
+                this.getSideBarToggleProperties.isSecondarySideNavOpen &&
+                isMobile
+            ) {
+                this.changeSidebarProperties();
+                this.changeSecondarySidebarProperties();
+            } else if (
+                this.getSideBarToggleProperties.isSideNavOpen &&
+                this.getSideBarToggleProperties.isSecondarySideNavOpen
+            ) {
+                this.changeSecondarySidebarProperties();
+            } else if (this.getSideBarToggleProperties.isSideNavOpen) {
+                this.changeSidebarProperties();
+            } else if (
+                !this.getSideBarToggleProperties.isSideNavOpen &&
+                !this.getSideBarToggleProperties.isSecondarySideNavOpen &&
+                !this.getSideBarToggleProperties.isActiveSecondarySideNav
+            ) {
+                this.changeSidebarProperties();
+            } else if (
+                !this.getSideBarToggleProperties.isSideNavOpen &&
+                !this.getSideBarToggleProperties.isSecondarySideNavOpen
+            ) {
+                this.changeSidebarProperties();
+                this.changeSecondarySidebarProperties();
+            }
+        },
+    },
 };
 </script>
-
-
-
